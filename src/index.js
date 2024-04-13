456668be30915683da1b055c012266e9071198ea
kutra89 <haloraziel@gmail.com>
2024-04-13 15:07:08 -0500

    first pull

---
+++ src/index.js
@@ -0,0 +1,5 @@
+let name = document.getElementById('nombre');
+let email = document.getElementById('email');
+let asunto = document.getElementById('asunto');
+let mensaje = document.getElementById('mensaje');
+
